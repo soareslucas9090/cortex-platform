ab01ad40db95bb416f928634bf1d3dd2477e7fcb
Lucas Soares <soareslukas9090@gmail.com>
2026-05-13 17:22:50 -0300

    refactor: renomeia state_class para state_class_builder e atualiza referências no ModelStateMixin

--- AppCore/core/state/state_mixin.py
+++ AppCore/core/state/state_mixin.py
@@ -1,6 +1,6 @@
 class ModelStateMixin:
     _state = None
-    state_class = None
+    state_class_builder = None
 
     @property
     def state(self):
@@ -10,15 +10,15 @@ class ModelStateMixin:
         return self._state
 
     def get_model_state_class(self):
-        if not self.state_class:
-            raise ValueError('state_class não foi definido no model')
+        if not self.state_class_builder:
+            raise ValueError('state_class_builder não foi definido no model')
 
-        return self.dispatcher_state_class.get(self.status)(object_instance=self)
+        return self.state_class_builder.get(self.status)(object_instance=self)
     
     def set_state(self, new_state):
-        if not self.state:
-            raise ValueError('state_class não foi definido no model')
+        if not self.state_class_builder:
+            raise ValueError('state_class_builder não foi definido no model')
         
         self.status = new_state
         self.save()
-        self.state
+        self._state = self.get_model_state_class()
