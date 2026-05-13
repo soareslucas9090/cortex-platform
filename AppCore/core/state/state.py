fec752a763adf894ff433ad4f6b1969adac6c3da
Lucas Soares <soareslukas9090@gmail.com>
2026-05-13 17:24:23 -0300

    feat: adiciona métodos pode_editar e pode_excluir na classe ModelInstanceState

--- AppCore/core/state/state.py
+++ AppCore/core/state/state.py
@@ -1,3 +1,9 @@
 class ModelInstanceState:
     def __init__(self, object_instance=None):
         self.object_instance = object_instance
+
+    def pode_editar(self):
+        raise NotImplementedError('O método pode_editar deve ser implementado na classe de estado')
+    
+    def pode_excluir(self):
+        raise NotImplementedError('O método pode_excluir deve ser implementado na classe de estado')
